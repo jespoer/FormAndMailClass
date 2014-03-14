f81f1ccd39f9302b6a5614dc774c4faa5534e9be
Jesper <jespoer@gmail.com>
2014-03-14 18:03:24 +0100

    Fixed code for sending email

--- form_to_mail.php
+++ form_to_mail.php
@@ -6,7 +6,32 @@ class FormMail{
 	
 	}
 	
-	public function send_mail(){
+	public function send_mail($first_name, $last_name, $address, $area, $phone, $email, $square_meter, $type){
+		
+		/* append input to a message */
+		$msg = "Ni har fått en ny offertförfrågan av : <b>"$first_name." ".$last_name."</b><br>
+				<b>Typ av tak : </b>".$type."<br>
+				<b>Takyta : </b>".$square_meter."<br>
+				<b>Adress : </b>".$address."<br>
+				<b>Område : </b>".$area."<br>
+				<b>Telefonnummer : </b>".$phone."<br>
+				<b>Epost : </b>".$email;
+		
+		/* additional headers. Needed, see php manual for more info */
+		$headers = "From : no-reply@takbytarna.se"."\r\n"."X-Mailer: PHP/".phpversion();
+		
+		$result = mail('jespoer@gmail.com', 'Takbytarna.se - Automatisk offertförfrågan', $msg, $headers);
+		
+		/* check result */
+		if($result == true){
+
+			/* return success */
+			return 1;
+		}else{
+		
+			/* return failure */
+			return 0;
+		}
 		
 	}
 }
--- redirect.php
+++ redirect.php
@@ -17,8 +17,13 @@ class Redirect{
 	}
 	
 	private function redirect_form_mail(){
-		if($this->session->send_mail()){
-			header('Location:main.php?success=true');
+		if(isset($_POST['email']) && isset($_POST['email_repeat']) && isset($_POST['name']) && isset($_POST['surname']) && $_POST['email'] == $_POST['email_repeat']){
+		
+			if($this->session->send_mail($_POST['name'], $_POST['surname'], $_POST['address'], $_POST['area'], $_POST['phone'], $_POST['mail'], $_POST['square_meter'], $_POST['type']) == 1){
+				header('Location:main.php?success=true');
+			}else{
+				header('Location:main.php?err=3'); 
+			}
 		}else{
 			header('Location:main.php?err=2');
 		}
